31a1df889b2541f1be2ddf1f79f21d3f9fd357be
PERSEO2000 <119625141+PERSEO2000@users.noreply.github.com>
2023-05-11 20:30:06 -0600

    2 files added

---
+++ src/comp/git.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./css/git.css"
+
+const Git = () => {
+  return (
+    <div>
+      <div>
+        <img src="../MULTIMEDIA/png-transparent-github-social-media-computer-icons-logo-android-github-logo-computer-wallpaper-banner-thumbnail (1).png">
+      </div>
+    </div>
+  )
+}
+
+export default Git
