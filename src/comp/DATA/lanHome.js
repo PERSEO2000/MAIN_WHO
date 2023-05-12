01625ad92e7b52056551973a4c3af1a8fb099427
PERSEO2000 <119625141+PERSEO2000@users.noreply.github.com>
2023-05-11 21:53:11 -0600

    2 files modified

--- src/comp/DATA/lanHome.js
+++ src/comp/DATA/lanHome.js
@@ -15,3 +15,22 @@ export const lanHome = [
     name: "TypeScript",
   }
 ]
+
+
+export const franeHome = [
+  {
+    id: 0,
+    img: "https://icon2.cleanpng.com/20180604/iqs/kisspng-react-javascript-angularjs-ionic-atom-5b154be64e7965.3537065815281223423214.jpg",
+    name: "React"
+  },
+  {
+    id: 1,
+    img: "https://cdn.icon-icons.com/icons2/3913/PNG/512/fastapi_logo_icon_248575.png",
+    name: "FastAPI"
+  },
+  {
+    id: 2,
+    img: "https://cdn.iconscout.com/icon/free/png-256/free-django-11-1175036.png?f=avif&w=256",
+    name: "Django"
+  },
+]
