3b391e04a8b473d90bbea68ddc8d372c7d619af8
PERSEO2000 <119625141+PERSEO2000@users.noreply.github.com>
2023-05-11 20:54:29 -0600

    2 files modified

--- src/comp/git.jsx
+++ src/comp/git.jsx
@@ -7,6 +7,9 @@ const Git = () => {
       <div>
         <img src="https://cdn.iconscout.com/icon/free/png-256/free-github-2752182-2284999.png?f=avif&w=256"/>
       </div>
+      <div>
+        <p>Aunque actualmente no tengo proyectos disponibles para mostrar, quiero asegurarte que siempre estoy trabajando en mejorar mis habilidades y conocimientos en programación. Si necesitas que suba algún proyecto específico, estaré más que dispuesto/a a hacerlo sin problema</p>
+      </div>
     </div>
   )
 }
